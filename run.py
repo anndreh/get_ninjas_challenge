5500ff8def6d70252a740c3187bf046e5eadb5e5
Andre Martins <anndreh@gmail.com>
2019-03-02 12:50:51 -0300

    Creates plateau object before rover moves

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 import argparse
-from app.move_rover import MoveRover
+from app.move_rover import RoversPlateau
 
 
-def main(args):
+def main(upper_right):
+    RoversPlateau(upper_right)
     print('Now inform the rover position or press "q" to quit.')
     rover_position = ''
     while rover_position != 'q':
         rover_position = raw_input('Rover position: ')
         rover_instructions = raw_input('Rover instructions: ')
-        print(MoveRover(rover_position, rover_instructions)
-            .move().getNewPosition())
+        RoversPlateau.move_rover(rover_position, rover_instructions)
 
 if __name__ == '__main__':
     parse = argparse.ArgumentParser(description='Robot rovers movements')
     parse.add_argument('--upper_right',
                        nargs='+', help='Upper right plateau', required=True)
-    args = parse.parse_args()
-    main(args)
+    upper_right_arg = parse.parse_args()
+    main(upper_right_arg)
