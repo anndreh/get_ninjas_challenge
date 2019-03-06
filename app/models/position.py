5a5669971e143e1d8e3d6c9e2c0db0fda87593c8
Andre Martins <anndreh@gmail.com>
2019-03-06 10:42:24 -0300

    Adds methods to move

--- app/models/position.py
+++ app/models/position.py
@@ -11,3 +11,25 @@ class Position:
             'y': self.y,
             'm': self.m
         }
+
+    def move_vertically(self, limits):
+        if self.m == 'N':
+            self.y += 1
+        else:
+            self.y -= 1
+        self.y = self.check_limits('y', self.y, limits)
+
+    def move_horizontally(self, limits):
+        if self.m == 'E':
+            self.x += 1
+        else:
+            self.x -= 1
+        self.x = self.check_limits('x', self.x, limits)
+
+    def check_limits(self, coordinate, value, limits):
+        # Check if rover touched the upper or bottom line
+        if value > limits[coordinate]:
+            value = limits[coordinate]
+        elif value < 0:
+            value = 0;
+        return value
