845e56311b45a8147660781997d8135a8c3696bf
Andre Martins <anndreh@gmail.com>
2019-03-07 08:07:04 -0300

    Adds tests to check rover movements

---
+++ test/services/test_move_rover.py
@@ -0,0 +1,52 @@
+import unittest
+from app.models.position import Position
+from app.rovers_plateau import RoversPlateau
+from app.services.move_rover import MoveRover
+
+
+class TestMoveRover(unittest.TestCase):
+    def setUp(self):
+        self.upper_right = {'x': 5, 'y': 5}
+        self.data_position = {
+            'x': 1,
+            'y': 2,
+            'm': 'N'
+        }
+        self.data_instructions = {
+            'move_foward': 'M',
+            'turn_left': 'L',
+            'turn_right': 'R'
+        }
+        self.expected_after_move = {
+            'x': 1,
+            'y': 3,
+            'm': 'N'
+        }
+        self.expected_after_turn_left = {
+            'x': 1,
+            'y': 2,
+            'm': 'W'
+        }
+        self.expected_after_turn_right = {
+            'x': 1,
+            'y': 2,
+            'm': 'E'
+        }
+        self.position = Position(
+                self.data_position['x'],
+                self.data_position['y'],
+                self.data_position['m']
+            )
+        self.move_rover = MoveRover(self.position, self.upper_right)
+
+    def test_rover_move_foward(self):
+        self.move_rover.move()
+        self.assertEqual(self.expected_after_move, self.position.dict())
+
+    def test_rover_move_turn_left(self):
+        self.move_rover.turn('L')
+        self.assertEqual(self.expected_after_turn_left, self.position.dict())
+
+    def test_rover_move_turn_right(self):
+        self.move_rover.turn('R')
+        self.assertEqual(self.expected_after_turn_right, self.position.dict())
