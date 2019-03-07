d5d053b773fab72f2f7cabfec887fcee85c6d8fe
Andre Martins <anndreh@gmail.com>
2019-03-07 08:21:00 -0300

    Improves move rover tests

--- test/services/test_move_rover.py
+++ test/services/test_move_rover.py
@@ -7,11 +7,8 @@ from app.services.move_rover import MoveRover
 class TestMoveRover(unittest.TestCase):
     def setUp(self):
         self.upper_right = {'x': 5, 'y': 5}
-        self.data_position = {
-            'x': 1,
-            'y': 2,
-            'm': 'N'
-        }
+        self.data_position = [1, 2, 'N']
+        self.data_position_limits = [5, 5, 'E']
         self.data_instructions = {
             'move_foward': 'M',
             'turn_left': 'L',
@@ -32,21 +29,33 @@ class TestMoveRover(unittest.TestCase):
             'y': 2,
             'm': 'E'
         }
-        self.position = Position(
-                self.data_position['x'],
-                self.data_position['y'],
-                self.data_position['m']
-            )
-        self.move_rover = MoveRover(self.position, self.upper_right)
+        self.expected_after_move_to_limit = {
+            'x': 5,
+            'y': 5,
+            'm': 'E'
+        }
 
     def test_rover_move_foward(self):
+        self.position = Position(*self.data_position)
+        self.move_rover = MoveRover(self.position, self.upper_right)
         self.move_rover.move()
         self.assertEqual(self.expected_after_move, self.position.dict())
 
     def test_rover_move_turn_left(self):
+        self.position = Position(*self.data_position)
+        self.move_rover = MoveRover(self.position, self.upper_right)
         self.move_rover.turn('L')
         self.assertEqual(self.expected_after_turn_left, self.position.dict())
 
     def test_rover_move_turn_right(self):
+        self.position = Position(*self.data_position)
+        self.move_rover = MoveRover(self.position, self.upper_right)
         self.move_rover.turn('R')
         self.assertEqual(self.expected_after_turn_right, self.position.dict())
+
+    def test_rover_move_to_limit(self):
+        self.position = Position(*self.data_position_limits)
+        self.move_rover = MoveRover(self.position, self.upper_right)
+        self.move_rover.move()
+        self.assertEqual(self.expected_after_move_to_limit,
+            self.position.dict())
