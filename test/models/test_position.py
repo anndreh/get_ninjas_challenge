602ef2de4b7536fe5dd2b97330ddd5fa64490cc2
Andre Martins <anndreh@gmail.com>
2019-03-07 08:10:19 -0300

    Improves the way insert data to setup

--- test/models/test_position.py
+++ test/models/test_position.py
@@ -4,7 +4,16 @@ from app.models.position import Position
 
 class TestPosition(unittest.TestCase):
     def setUp(self):
-        self.position = Position(x=1,y=2,m='N')
+        self.data = {
+            'x': 1,
+            'y': 2,
+            'm': 'N'
+        }
+        self.position = Position(
+                self.data['x'],
+                self.data['y'],
+                self.data['m']
+            )
 
     def test_position_x_is_not_none(self):
         self.assertIsNotNone(self.position.x)
