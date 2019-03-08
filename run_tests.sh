e4070a44662fbcdbd90e309edeca14fc702a4a6a
Andre Martins <anndreh@gmail.com>
2019-03-08 00:00:07 -0300

    Adds workaround solution to run tests into container

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo Creating new container
-docker run -d --name rover-ninja rover_challenge:1.0 /bin/bash
+docker create -ti --name rover-ninja rover_challenge:1.0
 echo Running test
+docker start rover-ninja
 docker exec rover-ninja python -m unittest discover
+echo Stopping container
+docker stop rover-ninja
 echo Removing old container
 docker rm rover-ninja
